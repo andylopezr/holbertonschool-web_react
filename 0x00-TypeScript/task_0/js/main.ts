d25097fe73c6e13ef9643702fd3fb0b0b2263237
Andy Lopez <profesor.de.ingles@aol.com>
2022-02-02 19:39:01 -0500

    |- ADDED -| project description and tasks 0 to 3

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,63 @@
+
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+
+}
+
+const student1: Student = {
+    firstName: "Ptarick",
+    lastName: "Jean luca",
+    age: 1,
+    location:"Paris sur mer"
+}
+const student2: Student = {
+    firstName: "Lisa",
+    lastName: "Sandrino",
+    age: 2,
+    location:"New York"
+}
+const headers: string[] = ["firstName", "location"];
+
+const studentsList: Array<Student> = [student1, student2];
+
+function generateTableHead(table: HTMLTableElement,headers: string[]): void {
+    const thead: HTMLTableSectionElement  = table.createTHead();
+    const row: HTMLTableRowElement   = thead.insertRow();
+
+    headers.forEach((headerTitle, index) =>{
+        const cell: HTMLTableDataCellElement = row.insertCell(index);
+        const text: Text = document.createTextNode(headerTitle);
+        cell.appendChild(text)
+        thead.appendChild(row)
+    })
+  }
+
+function generateTable(table: HTMLTableElement, data: Student[]): void{
+    const tbody: HTMLTableSectionElement = document.createElement("tbody");
+    table.appendChild(tbody);
+    data.forEach((element: Student)=> {
+        const row: HTMLTableRowElement = tbody.insertRow();
+        const cell1: HTMLTableDataCellElement = row.insertCell(0);
+        const text1: Text = document.createTextNode(element.firstName);
+        cell1.appendChild(text1)
+        const cell2: HTMLTableDataCellElement = row.insertCell(1);
+        const text2: Text = document.createTextNode(element.location);
+        cell2.appendChild(text2);
+    });
+}
+
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table: HTMLTableElement = document.createElement("table");
+body.appendChild(table)
+
+generateTableHead(table, headers);
+generateTable(table, studentsList);
+
+
+
+
+
+
